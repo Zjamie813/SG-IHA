720bc291d7a4ad5c21364428544133a062f29e29
Zjamie813 <53218176+Zjamie813@users.noreply.github.com>
2025-04-14 09:26:49 +0800

    Update run_irra_SG-IHA.sh

--- run_irra_SG-IHA.sh
+++ run_irra_SG-IHA.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python -u train.py --name 'dual_encoder_sdm_id_seed42'
+CUDA_VISIBLE_DEVICES=0 python -u train.py --name 'dual_encoder_added'
 --loss_names 'sdm+id'
 --dataset_name "CUHK-PEDES"
 --mcq_temperature 0.04
