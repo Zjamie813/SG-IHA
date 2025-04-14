c393e54f57f937f2f3fc64dbd9db7f5374aab9b9
Zjamie813 <53218176+Zjamie813@users.noreply.github.com>
2025-04-14 09:27:56 +0800

    Update run_clip_finetune_SG-IHA.sh

--- run_clip_finetune_SG-IHA.sh
+++ run_clip_finetune_SG-IHA.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python -u train.py --name 'dual_encoder_sdm_id_seed42'
---loss_names 'sdm+id'
+CUDA_VISIBLE_DEVICES=0 python -u train.py --name 'dual_encoder_added'
+--loss_names 'sdm+id+mcq'
 --dataset_name "CUHK-PEDES"
 --mcq_temperature 0.04
 
